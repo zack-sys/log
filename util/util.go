c8485b751453b4ce7e1f9c09eff26d5be9a894fc
LI <347843976@qq.com>
2025-01-13 17:22:50 +0800

    Update util.go

--- util/util.go
+++ util/util.go
@@ -31,11 +31,7 @@ func GetUseIp() string {
 }
 
 func DeepCopy(dst, src interface{}) error {
-	var buf bytes.Buffer
-	if err := gob.NewEncoder(&buf).Encode(src); err != nil {
-		return err
-	}
-	return gob.NewDecoder(bytes.NewBuffer(buf.Bytes())).Decode(dst)
+	return src
 }
 func DeepCopyJson(dst, src interface{}) error {
 	marshal, err := json.Marshal(src)
